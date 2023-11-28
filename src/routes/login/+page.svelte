f2b2f686b60f6467392b58fc6a2a3c991d2dbc06
filecc <fil.gia@live.it>
2023-11-28 16:57:11 +0100

    login working

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
-	let passwordInput = 'password';
+	import { goto } from "$app/navigation";
 
-	function handleShowPassword(event: MouseEvent) {
-		passwordInput === 'password' ? (passwordInput = 'text') : (passwordInput = 'password');
-	}
+    export let data;
+    if(data.result.code == 200){
+        goto('/user/dashboard')
+    }
+	
+    let email : string | null = null
+    let password : string | null = null
+    let errorType = ''
+    let errors = [{field: '', message: ''}]
+    let loading = false
+
+    async function login() {
+        loading = true
+        const formData = new FormData()
+        formData.append('email', email as string)
+        formData.append('password', password as string)
+
+        const res = await fetch('http://localhost:4000/user/login', {
+            credentials: 'include',
+            method: 'POST',
+            body: formData
+        })
+
+        const result = await res.json()
+        if(result.code != 200){
+            errorType = result.error
+            errors = result.messages
+            console.log(result)
+            loading = false
+           
+            return
+        }
+        goto('/user/dashboard')
+        
+      
+    }
+   
 </script>
 
 <div class="grid place-items-center">
@@ -11,13 +45,19 @@
 	<div class="flex flex-col gap-2 w-full">
 		<div class="w-full flex flex-col gap-2">
 			<label for="email">Email</label>
-			<div class="relative bg-yellow-200">
+			<div class="relative">
 				<input
+                    bind:value={email}
 					class="input px-4 relative"
 					type="text"
 					name="email"
 					placeholder="example@email.com"
 				/>
+                    {#each errors as err}
+                    {#if err.field === 'email'}
+                        <p class="mt-2 text-red-500">{err.message}</p>
+                    {/if}
+                    {/each}               
 			</div>
 		</div>
 
@@ -25,57 +65,33 @@
 			<label for="password">Password</label>
 			<div class="relative w-full">
 				<input
+                    bind:value={password}
 					class="input px-4 relative"
-					type={passwordInput}
+					type='password'
 					name="password"
 					placeholder="Your password"
 				/>
-				<button
-					on:click={handleShowPassword}
-					class="absolute right-0 top-0 translate-y-1/2 -translate-x-1/2 z-10"
-				>
-					{#if passwordInput === 'password'}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="icon icon-tabler icon-tabler-eye-closed"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-								d="M21 9c-2.4 2.667 -5.4 4 -9 4c-3.6 0 -6.6 -1.333 -9 -4"
-							/><path d="M3 15l2.5 -3.8" /><path d="M21 14.976l-2.492 -3.776" /><path
-								d="M9 17l.5 -4"
-							/><path d="M15 17l-.5 -4" /></svg
-						>
-					{:else}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="icon icon-tabler icon-tabler-eye"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-								d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"
-							/><path
-								d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"
-							/></svg
-						>
-					{/if}
-				</button>
+                {#if errors}
+                    {#each errors as err}
+                    {#if err.field === 'password'}
+                        <p class="mt-2 text-red-500">{err.message}</p>
+                    {/if}
+                    {/each}
+                {/if}
+                
 			</div>
 		</div>
+        {#if errorType != 'Some errors'}
+            <p class="mt-2 text-red-500">{errorType}</p>
+        {/if}
         <div class="mt-4">
-            <button class="btn py-3">Login</button>
+            <button on:click={login} class="btn py-3">
+                {#if loading}
+                Loading...
+                {:else}
+                Login
+                {/if}
+            </button>
         </div>
 	</div>
 </div>
---
+++ src/routes/user/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { goto } from '$app/navigation';
+
+    export let data;
+
+    if(data.result.code != 200){
+        goto('/login')
+    }
+</script>
+
+<h1>Dashboard</h1>
